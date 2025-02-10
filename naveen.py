0c705c6a03bbfe61dcb251a9db8398a7141f9b2b
NaveenNekkanti <ndurganaveen11045@gmail.com>
2025-02-10 10:36:36 +0530

    10-02-2025 naveen first  commit

--- naveen.py
+++ naveen.py
@@ -1,5 +1,4 @@
 from pymongo import MongoClient
 
 uri = "mongodb+srv://naveennekkanti:naveennekkanti@cluster0.2zvzf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(uri)
-print(client.server_info())  # Check connection
+client = MongoClient(uri)  # Check connection
